9dc73de25181ac349700688f4d9939a8f14312bc
Rarra <147137558+RarraxS@users.noreply.github.com>
2025-04-08 19:47:38 +0200

    Lights added

---
+++ Assets/Scripts/Lights/LightsController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsController : MonoBehaviour
+{
+    LightsModel lightsModel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
