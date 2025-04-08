0190f95a864cc434476e9d790d6ade00fef6fbf0
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-04-08 15:36:36 +0200

    Lights Sheet
    Las variables del script Lights Sheet han sido creadas.

--- Assets/Scripts/Lights/LightsSheet.cs
+++ Assets/Scripts/Lights/LightsSheet.cs
@@ -1,17 +1,30 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LightsModel : MonoBehaviour
 {
-    
+    private Light2D sunlight;
+
+    [SerializeField] private List<DayState> sunnyDayStates, 
+                                    rainyDayStates;
+
+    [SerializeField] private float rainingProbability;
+
+    [SerializeField] private GameObject rainParticleEmmiter;
+
+    private bool raining = false;
+
+    private float timeTimer;
+
+    private int indexDayState = 0;
 }
 
 [Serializable]
 public class DayState
 {
-    public int hour;
+    public byte hour;
 
     public Color ilumination;
 }
